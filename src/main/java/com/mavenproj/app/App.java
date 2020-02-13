7dc470f861737d57676773670b3929065694edbe
Nagaraj <NNagarajece31@gmail.com>
2020-02-13 21:40:36 +0530

    ok http added

--- src/main/java/com/mavenproj/app/App.java
+++ src/main/java/com/mavenproj/app/App.java
@@ -1,11 +1,27 @@
 package com.mavenproj.app;
 
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        System.out.println(new App().run("https://square.github.io/okhttp/"));
     }
+
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder().url(url).build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
 }
